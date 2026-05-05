6982e89be9c810c62c156aee2abe98a3374bb676
Mustafa Afzal <104111690+mustafa-afzal@users.noreply.github.com>
2026-05-05 18:55:22 -0400

    Time: 15 ms (42.94%), Space: 19.4 MB (75.2%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,12 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         dictS = {}
+        dictT = {}
         for i in range(len(s)):
             if s[i] in dictS:
                 dictS[s[i]] += 1
             else:
                 dictS[s[i]] = 1
-        dictT = {}
         for j in range(len(t)):
             if t[j] in dictT:
                 dictT[t[j]] += 1
