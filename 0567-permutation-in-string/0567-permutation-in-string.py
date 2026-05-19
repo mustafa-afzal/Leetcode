417a7575f2e26e4b82bde5359ba9193003c473e2
Mustafa Afzal <104111690+mustafa-afzal@users.noreply.github.com>
2026-05-18 22:22:20 -0400

    Time: 15 ms (82.42%), Space: 19.5 MB (19.56%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,36 +1,27 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
+        if len(s2) < len(s1):
             return False
-        s1Count, s2Count = [0] * 26, [0] * 26
+        map1 = {}
+        map2 = {}
+        k = len(s1)
         for i in range(len(s1)):
-            s1Count[ord(s1[i]) - ord('a')] += 1
-            s2Count[ord(s2[i]) - ord('a')] += 1
-        matches = 0
-        for i in range(26):
-            if s1Count[i] == s2Count[i]:
-                matches += 1
+            if s1[i] in map1:
+                map1[s1[i]] += 1
             else:
-                continue
-        l = 0
-        for r in range(len(s1), len(s2)):
-            if matches == 26:
-                return True
-            index = ord(s2[r]) - ord('a')
-            s2Count[index] += 1
-            if s1Count[index] == s2Count[index]:
-                matches += 1
-            elif s1Count[index] + 1 == s2Count[index]:
-                matches -= 1
-            index = ord(s2[l]) - ord('a')
-            s2Count[index] -= 1
-            if s1Count[index] == s2Count[index]:
-                matches += 1
-            elif s1Count[index] - 1 == s2Count[index]:
-                matches -= 1
-            l += 1
-        return matches == 26
-
-        
-
-            
+                map1[s1[i]] = 1
+        for i in range(k):
+            if s2[i] in map2:
+                map2[s2[i]] += 1
+            else:
+                map2[s2[i]] = 1
+        for i in range(0, len(s2) - k):
+            if map1 == map2: return True
+            if s2[i+k] in map2:
+                map2[s2[i+k]] += 1
+            else:
+                map2[s2[i+k]] = 1
+            map2[s2[i]] -= 1
+            if map2[s2[i]] == 0:
+                del map2[s2[i]]
+        return map1 == map2
