dfb15cdb0d1ec934e5a797e14659aa610870c8a8
Mustafa Afzal <104111690+mustafa-afzal@users.noreply.github.com>
2026-05-21 13:07:44 -0400

    Time: 103 ms (42.74%), Space: 19.6 MB (50.92%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,18 +1,19 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         l = 0
+        map = {}
         res = 0
-        dict1 = {}
         for r in range(len(s)):
-            if s[r] in dict1:
-                dict1[s[r]] += 1
+            if s[r] in map:
+                map[s[r]] += 1
             else:
-                dict1[s[r]] = 1
+                map[s[r]] = 1
+            maxFreq = max(map.values())
             windowSize = r - l + 1
-            maxFreq = max(dict1.values())
             if windowSize - maxFreq > k:
-                dict1[s[l]] -= 1
+                map[s[l]] -= 1
                 l += 1
             else:
                 res = max(res, windowSize)
         return res
+            
