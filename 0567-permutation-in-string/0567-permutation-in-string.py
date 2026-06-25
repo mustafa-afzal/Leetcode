ae925144eb636ca6c75d89cd6b01ea9ba6584ce0
Mustafa Afzal <104111690+mustafa-afzal@users.noreply.github.com>
2026-06-25 13:40:11 -0400

    Time: 46 ms (30.71%), Space: 19.5 MB (18.07%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,27 +1,17 @@
+from collections import Counter
+
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s2) < len(s1):
-            return False
-        map1 = {}
-        map2 = {}
+        map1 = Counter(s1)
         k = len(s1)
-        for i in range(len(s1)):
-            if s1[i] in map1:
-                map1[s1[i]] += 1
-            else:
-                map1[s1[i]] = 1
-        for i in range(k):
-            if s2[i] in map2:
-                map2[s2[i]] += 1
-            else:
-                map2[s2[i]] = 1
+        map2 = Counter(s2[:k])
+        if map1 == map2:
+            return True
         for i in range(0, len(s2) - k):
-            if map1 == map2: return True
-            if s2[i+k] in map2:
-                map2[s2[i+k]] += 1
-            else:
-                map2[s2[i+k]] = 1
             map2[s2[i]] -= 1
             if map2[s2[i]] == 0:
                 del map2[s2[i]]
-        return map1 == map2
+            map2[s2[i + k]] += 1
+            if map1 == map2:
+                return True
+        return False
